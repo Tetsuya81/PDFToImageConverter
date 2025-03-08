a8f651a7dee886bb7097ca834166d55fb24b5200
Tetsuya81 <tetsuya8109@gmail.com>
2025-03-08 16:23:04 +0900

    Initial Commit

---
+++ PDFToImageConverter/PDFToImageConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFToImageConverterApp.swift
+//  PDFToImageConverter
+//
+//  Created by Tokunaga Tetsuya on 2025/03/08.
+//
+
+import SwiftUI
+
+@main
+struct PDFToImageConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PDFToImageConverterTests/PDFToImageConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  PDFToImageConverterTests.swift
+//  PDFToImageConverterTests
+//
+//  Created by Tokunaga Tetsuya on 2025/03/08.
+//
+
+import Testing
+@testable import PDFToImageConverter
+
+struct PDFToImageConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
